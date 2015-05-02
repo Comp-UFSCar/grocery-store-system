c8d91b85bfd9cbcc106396c9204843a30d1f5ba3
mrtakata <matheustakata@Matheuss-MacBook-Pro.local>
2015-05-02 15:29:20 -0300

    setEmail added, needs to be fixed; added test case file for class Cliente.

--- src/projetomercado/Cliente.java
+++ src/projetomercado/Cliente.java
@@ -2,44 +2,93 @@ package projetomercado;
 
 public class Cliente {
 	private String nome;
-	private String CPF;
+	private String cpf;
 	private String endereco;
 	private String telefone;
 	private String email;
 	private int status;
 	
-	Cliente(String nome, String CPF, String endereco, String telefone, 
+	public static final int ATIVO = 1;
+	public static final int INATIVO = 0;
+	
+	public Cliente(String nome, String cpf, String endereco, String telefone, 
 			String email, int status){
-		
+		setNome(nome);
+		setCpf(cpf);
+		setEndereco(endereco);
+		setTelefone(telefone);
+		setEmail(email);
+		setStatus(status);
 	}
 	
-	private void validaNome(String nome){
-		
+	//getters
+	public String nome(){
+		return this.nome;
 	}
 	
-	private void validaCPF(String CPF){
-		
+	public String getCpf(){
+		return this.cpf;
 	}
 	
-	private void validaEndereco(String endereco){
-		
+	public String getEndereco(){
+		return this.endereco;
 	}
 	
-	private void validaTelefone(String telefone){
-		
+	public String getTelefone(){
+		return this.telefone;
 	}
 	
-	private void validaEmail(String email){
-		
+	public String getEmail(){
+		return this.email;
 	}
 	
-	private void validaStatus(int status){
-		
+	public int getStatus(){
+		return this.status;
 	}
 	
-	public void alterarCliente(String nome, String CPF, String endereco, String telefone, 
+	//setters
+	public void setNome(String nome){
+		this.nome = nome;
+	}
+	
+	public void setCpf(String cpf){
+		this.cpf = cpf;
+	}
+	
+	public void setEndereco(String endereco){
+		this.endereco = endereco;
+	}
+	
+	public void setTelefone(String telefone){
+		this.telefone = telefone;
+	}
+	
+	public void setEmail(String email){
+		//corrigir
+		//matches letter + alphanumeric(s) + @ + letter + alphanumeric(s) + ".com" (validating only if the following characters are ".br")
+		try{
+			if(!email.matches("([a-z])([a-z]|\\d)*@([a-z])([a-z]|\\d)*(.com)(?=.br)"))
+				throw new RuntimeException("E-mail invalido " + email);
+			
+			this.email = email;
+		}
+		catch (RuntimeException re){
+			System.err.println(re);
+		}
+	}
+	
+	public void setStatus(int status){
+		this.status = status;
+	}
+	
+	public void alteraCliente(String nome, String CPF, String endereco, String telefone, 
 			String email, int status){
-		
+		setNome(nome);
+		setCpf(cpf);
+		setEndereco(endereco);
+		setTelefone(telefone);
+		setEmail(email);
+		setStatus(status);
 	}
 	
 	
---
+++ src/testes/TesteCliente.java
@@ -0,0 +1,36 @@
+package testes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import projetomercado.Cliente;
+
+public class TesteCliente {
+	
+	//To be done correctly
+	@Test
+	public void emailHasNoUser() {
+		Cliente c = new Cliente("nome", "cpf", "endereco", "(14)1234-5678", "@a.com", 0);
+	}
+	@Test
+	public void emailsHasNoDomain() {
+		Cliente c = new Cliente("nome", "cpf", "endereco", "(14)1234-5678", "a@.com", 0);
+	}
+	@Test
+	public void emailHasNotAcceptableComplement() {
+		Cliente c = new Cliente("nome", "cpf", "endereco", "(14)1234-5678", "a@a.com.uk", 0);
+	}
+	@Test
+	public void userStartsWithNumber() {
+		Cliente c = new Cliente("nome", "cpf", "endereco", "(14)1234-5678", "1ab@a.com", 0);
+	}
+	@Test
+	public void domainStartsWithNumber() {
+		Cliente c = new Cliente("nome", "cpf", "endereco", "(14)1234-5678", "bla@1do.com", 0);
+	}
+	@Test
+	public void test() {
+		Cliente c = new Cliente("nome", "cpf", "endereco", "(14)1234-5678", "a@a.com", 0);
+	}
+}
