daec61ea9e967f40b18daaf6d6dcbabbe8268476
thamenato <thamenato@gmail.com>
2015-05-01 22:27:25 -0300

    Produto e GerenciamentoProdutos funcionais
    Produto: getters e setters estao criados e com as regras implementadas.
    GerenciamentoProdutos: controle dos produtos e realizado nesta classe, possuindo uma lista de arrays de produto para controle de codigos existentes cadastrados.
    Mercado: em main() ha alguns objetos de teste.

--- src/projetomercado/Mercado.java
+++ src/projetomercado/Mercado.java
@@ -3,15 +3,31 @@ package projetomercado;
 import java.util.Vector;
 
 public class Mercado {
-	private Vector<Produto> produtos;
-	private Vector<Cliente> clientes;
-	private Vector<RegistroVenda> registrosVendas;
-	
+
+	//private Vector<Produto> produtos;
+	//private Vector<Cliente> clientes;
+	//private Vector<RegistroVenda> registrosVendas;
+
+	static GerenciamentoProdutos gProdutos = new GerenciamentoProdutos();
+
+	public static void main(String args[])
+	{
+		gProdutos.insereProduto(11111, "este eh o produto 1", 1.00, 5.99, "cm", 10.5, 1);
+		//gProdutos.insereProduto(22222, "e este eh o produto 2", 2.00, 7.99, "kg", 10.5, 1);
+		//gProdutos.insereProduto(33333, "por fim o produto 3", 3.00, 10.99, "mm", 10.5, 1);
+		gProdutos.listaProdutos();
+		gProdutos.alteraEstoque(11111, 0);
+		gProdutos.listaProdutos();
+	}
+
+	// TODO: calculaFaturamento
 	public double calculaFaturamento(int mes){
 		return 0.0f;
 	}
-	
+
+	// TODO: consultaProdutos
 	public int[] consultaProdutos(String descricao){
-		
+		return null;
 	}
+
 }
--- src/projetomercado/Produto.java
+++ src/projetomercado/Produto.java
@@ -1,6 +1,7 @@
 package projetomercado;
 
 public class Produto {
+
 	private int codigo;
 	private String descricao;
 	private double precoCompra;
@@ -8,39 +9,137 @@ public class Produto {
 	private String unidade;
 	private double estoque;
 	private int status;
-	
-	Produto (int codigo, String descricao, double precoCompra, double precoVenda, 
-			String unidade, double estoque, int status){
-		
-	}
-	
-	public void alteraProduto (int codigo, String descricao, double precoCompra, double precoVenda, 
-			String unidade, double estoque, int status){
-		
-	}
-	
-	public void alteraEstoque(int codigo, double estoque){
-		
-	}
-	
-	private void validaCodigo(int codigo){
-		
-	}
-	
-	private void validaDescricao(String descricao){
-		
-	}
-	
-	private void validaPreco(String Preco){
-		
-	}
-	
-	private void validaUnidate(String unidade){
-		
-	}
-	
-	private void validaEstoque(double estoque){
-		
+
+	Produto (int codigo, String descricao, double precoCompra, double precoVenda,
+			 String unidade, double estoque, int status){
+
+		setCodigo(codigo);
+		setDescricao(descricao);
+		setPrecoCompra(precoCompra);
+		setPrecoVenda(precoVenda);
+		setUnidade(unidade);
+		setEstoque(estoque);
+		setStatus(status);
 	}
 
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		try {
+			if(codigo <= 0)
+				throw new RuntimeException("Codigo invalido " + codigo);
+
+			this.codigo = codigo;
+		}
+		catch (RuntimeException re){
+			System.err.println(re);
+		}
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		try{
+			if(!(descricao.matches("[\\w|\\s]{3,64}")))
+				throw new RuntimeException("Descricao invalida: " + descricao);
+
+			this.descricao = descricao;
+		}
+		catch (RuntimeException re){
+			System.err.println(re);
+		}
+	}
+
+	public double getPrecoCompra() {
+		return precoCompra;
+	}
+
+	public void setPrecoCompra(double precoCompra) {
+		try{
+			if(precoCompra <= 0)
+				throw new RuntimeException("Preco de compra invalido: " + precoCompra);
+
+			this.precoCompra = precoCompra;
+		} catch (RuntimeException re){
+			System.err.println(re);
+		}
+	}
+
+	public double getPrecoVenda() {
+		return precoVenda;
+	}
+
+	public void setPrecoVenda(double precoVenda) {
+		try{
+			if(precoVenda < this.precoCompra)
+				throw new RuntimeException("Preco de venda invalido: " + precoVenda);
+
+			this.precoVenda = precoVenda;
+		} catch (RuntimeException re){
+			System.err.println(re);
+		}
+		this.precoVenda = precoVenda;
+	}
+
+	public String getUnidade() {
+		return unidade;
+	}
+
+	public void setUnidade(String unidade) {
+		try{
+			if(!(unidade.matches("[a-zA-Z]{2}")))
+				throw new RuntimeException("Unidade invalida: " + unidade);
+
+			this.unidade = unidade;
+		}catch(RuntimeException re){
+			System.err.println(re);
+		}
+	}
+
+	public double getEstoque() {
+		return estoque;
+	}
+
+	public void setEstoque(double estoque) {
+		try{
+			if(estoque < 0)
+				throw new RuntimeException("Estoque invalido: " + estoque);
+
+			this.estoque = estoque;
+		} catch(RuntimeException re){
+			System.err.println(re);
+		}
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		try {
+			if (!(status == 1 || status == 0))
+				throw new RuntimeException("Status - valor invalido: " + status);
+			if (status == 0 && this.estoque > 0)
+				throw new RuntimeException("Status - nao pode ser desativado, ainda ha estoque");
+
+			this.status = status;
+		} catch (RuntimeException re) {
+			System.err.println(re);
+		}
+	}
+
+	public void alteraProduto (int codigo, String descricao, double precoCompra, double precoVenda,
+							   String unidade, double estoque, int status){
+		setCodigo(codigo);
+		setDescricao(descricao);
+		setPrecoCompra(precoCompra);
+		setPrecoVenda(precoVenda);
+		setUnidade(unidade);
+		setEstoque(estoque);
+		setStatus(status);
+	}
 }
