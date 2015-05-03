5e862a2a574f7bc6ed1b8e72034501cdd8aba68f
mrtakata <matheustakata@Matheuss-MacBook-Pro.local>
2015-05-03 19:19:32 -0300

    adding setCPF method

--- src/projetomercado/Cliente.java
+++ src/projetomercado/Cliente.java
@@ -48,26 +48,97 @@ public class Cliente {
 	
 	//setters
 	public void setNome(String nome){
+		/*
+		try{
+			if(!telefone.matches("[a-zA-Z\\-'\\s]+"))
+				throw new RuntimeException("Nome invalido " + nome);
+			
+			this.nome = nome;
+		}
+		catch (RuntimeException re){
+			System.err.println(re);
+		}
+		*/
 		this.nome = nome;
 	}
 	
 	public void setCpf(String cpf){
-		this.cpf = cpf;
+		try{
+			if (cpf.equals("00000000000") || cpf.equals("11111111111") || 
+				cpf.equals("22222222222") || cpf.equals("33333333333") || 
+				cpf.equals("44444444444") || cpf.equals("55555555555") || 
+				cpf.equals("66666666666") || cpf.equals("77777777777") ||
+				cpf.equals("88888888888") || cpf.equals("99999999999") || 
+				(cpf.length() != 11))
+				throw new RuntimeException("CPF invalido: " + cpf);
+			
+			int dig10, dig11, sum = 0;
+			//10th digit verification
+			for(int i = 0; i < 9; i++){
+				sum += Character.getNumericValue( cpf.charAt(i) ) * (10-i);
+			}
+			dig10 = 11 - sum % 11;
+			
+			if (dig10 == 10 || dig10 == 11){
+				dig10 = 0;
+			}
+			
+			if( Character.getNumericValue( cpf.charAt(9) ) != dig10){
+				throw new RuntimeException("CPF invalido: " + cpf);
+			}
+			
+			//11th digit verification
+			sum = 0;
+			for(int i = 0; i < 10; i++){
+				sum += Character.getNumericValue( cpf.charAt(i) ) * (11-i);
+			}
+			dig11 = 11 - sum % 11;
+			
+			if (dig11 == 10 || dig11 == 11){
+				dig11 = 0;
+			}
+			if( Character.getNumericValue( cpf.charAt(10) ) != dig11){
+				throw new RuntimeException("CPF invalido: " + cpf);
+			}
+			this.cpf = cpf;
+		}
+		catch (RuntimeException re){
+			System.err.println(re);
+		}
 	}
 	
 	public void setEndereco(String endereco){
+		/*
+		try{
+			if(!telefone.matches("\\w{1,256}"))
+				throw new RuntimeException("endereco invalido " + endereco);
+			
+			this.endereco = endereco;
+		}
+		catch (RuntimeException re){
+			System.err.println(re);
+		}
+		*/
 		this.endereco = endereco;
 	}
 	
 	public void setTelefone(String telefone){
-		this.telefone = telefone;
+		try{
+			if(!telefone.matches("\\(\\d{2}\\)\\d{9}"))
+				throw new RuntimeException("telefone invalido " + telefone);
+			
+			this.telefone = telefone;
+		}
+		catch (RuntimeException re){
+			System.err.println(re);
+		}
 	}
 	
 	public void setEmail(String email){
 		//corrigir
 		//matches letter + alphanumeric(s) + @ + letter + alphanumeric(s) + ".com" (validating only if the following characters are ".br")
 		try{
-			if(!email.matches("([a-z])([a-z]|\\d)*@([a-z])([a-z]|\\d)*.com(.br)*"))
+			if(!email.matches("([a-z])([a-z]|\\d)*@([a-z])([a-z]|\\d)*.com(.br){0,1}"))
 				throw new RuntimeException("E-mail invalido " + email);
 			
 			this.email = email;
--- src/testes/TesteCliente.java
+++ src/testes/TesteCliente.java
@@ -7,35 +7,148 @@ import org.junit.Test;
 import projetomercado.Cliente;
 
 public class TesteCliente {
-	
+	//Note: cpf generated from http://www.geradorcpf.com/
+	static Cliente c = new Cliente("Lu Bu", "15988150241", "endereco", "(14)123456789", "a@a.com", 0);
 	//To be done correctly
+	
+	//nome tests
+	
+	@Test
+	public void nomeIsEmpty(){
+		c.setNome("");
+	}
+	@Test
+	public void nomeIsTooShort(){
+		c.setNome("Monty Python");
+	}
+
+	/*
+	//cpf tests
+	@Test
+	public void cpfIsTooShort(){
+		c.setCpf("1111111111");
+	}
+	@Test
+	public void cpfIsTooLong(){
+		c.setCpf("11111111111");
+	}
+	@Test
+	public void cpfHasNonDigits(){
+		c.setCpf("1111111111a");
+	}
+	@Test
+	public void cpfIsAll0s(){
+		c.setCpf("00000000000");
+	}
+	@Test
+	public void cpfIsAll1s(){
+		c.setCpf("11111111111");
+	}
+	@Test
+	public void cpfIsAll2s(){
+		c.setCpf("22222222222");
+	}
+	@Test
+	public void cpfIsAll3s(){
+		c.setCpf("33333333333");
+	}
+	@Test
+	public void cpfIsAll4s(){
+		c.setCpf("44444444444");
+	}
+	@Test
+	public void cpfIsAll5s(){
+		c.setCpf("55555555555");
+	}
+	@Test
+	public void cpfIsAll6s(){
+		c.setCpf("66666666666");
+	}
+	@Test
+	public void cpfIsAll7s(){
+		c.setCpf("77777777777");
+	}
+	@Test
+	public void cpfIsAll8s(){
+		c.setCpf("88888888888");
+	}
+	@Test
+	public void cpfIsAll9s(){
+		c.setCpf("99999999999");
+	}
+	@Test
+	public void cpfHas10thDigitWrong(){
+		c.setCpf("15988150231");
+	}
+	@Test
+	public void cpfHas11thDigitWrong(){
+		c.setCpf("15988150242");
+	}
+	@Test
+	public void cpfValid(){
+		c.setCpf("15988150241");
+	}
+	
+	//telefone tests
+	@Test
+	public void telHasMoreThan10digits(){
+		c.setTelefone("(14)12345678910");
+	}
+	
+	@Test
+	public void telHasLessThan9digits(){
+		c.setTelefone("(14)12345678");
+	}
+	
+	@Test
+	public void telHasNoParenthesis(){
+		c.setTelefone("14123456789");;
+	}
+	
+	@Test
+	public void telHasDash(){
+		c.setTelefone("(14)12345-6789");;
+	}
+	
+	@Test
+	public void correctInput(){
+		c.setTelefone("(11)123456789");;
+	}
+	
+	//e-mail tests
+	
 	@Test
 	public void emailHasNoUser() {
-		Cliente c = new Cliente("nome", "cpf", "endereco", "(14)1234-5678", "@a.com", 0);
-	}
-	@Test
-	public void emailsHasNoDomain() {
-		Cliente c = new Cliente("nome", "cpf", "endereco", "(14)1234-5678", "a@.com", 0);
-	}
-	@Test
-	public void emailHasNotAcceptableComplement() {
-		Cliente c = new Cliente("nome", "cpf", "endereco", "(14)1234-5678", "a@a.com.uk", 0);
-	}
-	@Test
-	public void userStartsWithNumber() {
-		Cliente c = new Cliente("nome", "cpf", "endereco", "(14)1234-5678", "1ab@a.com", 0);
-	}
-	@Test
-	public void domainStartsWithNumber() {
-		Cliente c = new Cliente("nome", "cpf", "endereco", "(14)1234-5678", "bla@1do.com", 0);
-	}
-	@Test
-	public void acceptableEmail1() {
-		Cliente c = new Cliente("nome", "cpf", "endereco", "(14)1234-5678", "a234@a.com.br", 0);
-	}
-	@Test
-	public void acceptableEmail2() {
-		Cliente c = new Cliente("nome", "cpf", "endereco", "(14)1234-5678", "a@a.com", 0);
+		c.setEmail("@a.com");
 	}
 	
+	@Test
+	public void emailsHasNoDomain() {
+		c.setEmail("a@.com");
+	}
+	@Test
+	public void emailHasNoAcceptableComplement1() {
+		c.setEmail("a@a.com.uk");
+	}
+	@Test
+	public void emailHasNoAcceptableComplement2() {
+		c.setEmail("a@a.co");
+	}
+	@Test
+	public void emailsUserStartsWithNumber() {
+		c.setEmail("1a@a.com");
+	}
+	@Test
+	public void emailsDomainStartsWithNumber() {
+		c.setEmail("a@1a.com");
+	}
+	@Test
+	public void emailAccepted1() {
+		c.setEmail("a@a.com");
+	}
+	@Test
+	public void emailAccepted2() {
+		c.setEmail("a@a.com.br");
+	}
+	*/
 }
