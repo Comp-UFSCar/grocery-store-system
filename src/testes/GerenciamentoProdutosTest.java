78b51d55517a02933e33d8575a2ab6c6429970ce
thamenato <thamenato@gmail.com>
2015-05-03 02:09:49 -0300

    Mudancas na classe Produto e criacao das classes de testes para Produto

---
+++ src/testes/GerenciamentoProdutosTest.java
@@ -0,0 +1,40 @@
+package testes;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GerenciamentoProdutosTest {
+
+
+
+    @Test
+    public void testInsereProduto() throws Exception {
+
+    }
+
+    @Test
+    public void testAlteraEstoque() throws Exception {
+
+    }
+
+    @Test
+    public void testGetProdutos() throws Exception {
+
+    }
+
+    @Test
+    public void testGetProduto() throws Exception {
+
+    }
+
+    @Test
+    public void testListaProdutos() throws Exception {
+
+    }
+
+    @Test
+    public void testExibeProduto() throws Exception {
+
+    }
+}
