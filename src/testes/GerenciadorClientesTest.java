572b3001d65506b14e158886ac41548b6ed72235
mrtakata <matheustakata@Matheuss-MacBook-Pro.local>
2015-06-16 21:00:16 -0300

    Fixing setEndereco and adding more tests to GerenciadorClientesTest.java

--- src/testes/GerenciadorClientesTest.java
+++ src/testes/GerenciadorClientesTest.java
@@ -8,10 +8,11 @@ import static org.junit.Assert.*;
 
 public class GerenciadorClientesTest {
 
+    public static Cliente c;
     @BeforeClass
     static public void before() throws Exception {
         GerenciadorClientes.insereCliente("Fabiane Ferrari", "12647527504", "endereco", "(14)123456789", "a@a.com", Cliente.ATIVO);
-
+        c = new Cliente("Fabiane Ferrari", "12647527504", "endereco", "(14)123456789", "a@a.com", Cliente.ATIVO);
     }
 
     // Incorrect inputs
@@ -21,23 +22,23 @@ public class GerenciadorClientesTest {
     }
     @Test (expected = RuntimeException.class)
     public void insereClienteFullIncorrectCpf() throws Exception {
-        GerenciadorClientes.insereCliente("Lu B", "5236194644", "endereco", "(14)123456789", "a@a.com", Cliente.ATIVO);
+        GerenciadorClientes.insereCliente("Lu Bu", "5236194644", "endereco", "(14)123456789", "a@a.com", Cliente.ATIVO);
     }
     @Test (expected = RuntimeException.class)
     public void insereClienteFullIncorrectEndereco() throws Exception {
-        GerenciadorClientes.insereCliente("Lu B", "52361946440", "--", "(14)123456789", "a@a.com", Cliente.ATIVO);
+        GerenciadorClientes.insereCliente("Lu Bu", "52361946440", "Rua Santos Dumont - 10", "(14)123456789", "a@a.com", Cliente.ATIVO);
     }
     @Test (expected = RuntimeException.class)
     public void insereClienteFullIncorrectTelefone() throws Exception {
-        GerenciadorClientes.insereCliente("Lu B", "52361946440", "endereco", "14123456789", "a@a.com", Cliente.ATIVO);
+        GerenciadorClientes.insereCliente("Lu Bu", "52361946440", "endereco", "14123456789", "a@a.com", Cliente.ATIVO);
     }
     @Test (expected = RuntimeException.class)
     public void insereClienteFullIncorrectEmail() throws Exception {
-        GerenciadorClientes.insereCliente("Lu B", "52361946440", "endereco", "(14)123456789", "a@.com", Cliente.ATIVO);
+        GerenciadorClientes.insereCliente("Lu Bu", "52361946440", "endereco", "(14)123456789", "a@.com", Cliente.ATIVO);
     }
     @Test (expected = RuntimeException.class)
     public void insereClienteFullIncorrectStatus() throws Exception {
-        GerenciadorClientes.insereCliente("Lu B", "52361946440", "endereco", "(14)123456789", "a@a.com", 2);
+        GerenciadorClientes.insereCliente("Lu Bu", "52361946440", "endereco", "(14)123456789", "a@a.com", 2);
     }
 
     @Test
@@ -46,25 +47,25 @@ public class GerenciadorClientesTest {
     }
 
     @Test (expected = RuntimeException.class)
+    public void insereClientesameCpfInput() throws Exception {
+        GerenciadorClientes.insereCliente("Fabiane Ferrari", "12647527504", "endereco", "(14)123456789", "a@a.com", Cliente.ATIVO);
+    }
+
+    @Test (expected = RuntimeException.class)
     public void insereClienteFullsameCpf() throws Exception {
         GerenciadorClientes.insereCliente("Fabiane Ferrari", "12647527504", "endereco", "(14)123456789", "a@a.com", Cliente.ATIVO);
     }
 
     @Test
-    public void testInsereClienteWithObjectcorrectInput() throws Exception {
-        Cliente c = new Cliente("Fabiano Ferrari", "15988150241", "endereco", "(14)123456789", "a@a.com", Cliente.ATIVO);
-        GerenciadorClientes.insereCliente(c);
+    public void testConsultaClientePorCpf() throws Exception {
+        assertNotNull(GerenciadorClientes.consultaClientePorCpf("12647527504"));
+        assertNull(GerenciadorClientes.consultaClientePorCpf("52361946440"));
+        assertNull(GerenciadorClientes.consultaClientePorCpf("1264752750"));
     }
 
     @Test
-    public void testConsultaClientePorCpf() throws Exception {
-        //TODO: Test goes here...
-        GerenciadorClientes.consultaClientePorCpf("12647527504");
-    }
-    @Test
-    public void testcpfInserido() throws Exception {
-        assertTrue(GerenciadorClientes.cpfInserido("12647527504"));
-        assertFalse(GerenciadorClientes.cpfInserido("49762871650"));
+    public void testGetClientes(){
+        assertNotNull(GerenciadorClientes.getClientes());
     }
 
 } 
